2ef7b960f8d467bb386f06be8993eb9f273ccd3d
doyaGu <doyaGu@users.noreply.github.com>
2025-11-12 22:17:53 -0500

    Improve logger system

--- src/core/logger.c
+++ src/core/logger.c
@@ -4,27 +4,27 @@
 
 #define NMO_LOG_BUFFER_SIZE 4096
 
-static const char* log_level_names[] = {
+static const char *log_level_names[] = {
     [NMO_LOG_DEBUG] = "DEBUG",
-    [NMO_LOG_INFO]  = "INFO",
-    [NMO_LOG_WARN]  = "WARN",
+    [NMO_LOG_INFO] = "INFO",
+    [NMO_LOG_WARN] = "WARN",
     [NMO_LOG_ERROR] = "ERROR",
 };
 
-static void stderr_log(void* user_data, nmo_log_level level, const char* message) {
-    (void)user_data;
+static void stderr_log(void *user_data, nmo_log_level_t level, const char *message) {
+    (void) user_data;
     fprintf(stderr, "[%s] %s\n", log_level_names[level], message);
 }
 
-static void null_log(void* user_data, nmo_log_level level, const char* message) {
-    (void)user_data;
-    (void)level;
-    (void)message;
+static void null_log(void *user_data, nmo_log_level_t level, const char *message) {
+    (void) user_data;
+    (void) level;
+    (void) message;
     // Discard all messages
 }
 
-nmo_logger nmo_logger_stderr(void) {
-    nmo_logger logger = {
+nmo_logger_t nmo_logger_stderr(void) {
+    nmo_logger_t logger = {
         .log = stderr_log,
         .user_data = NULL,
         .level = NMO_LOG_INFO
@@ -32,8 +32,8 @@ nmo_logger nmo_logger_stderr(void) {
     return logger;
 }
 
-nmo_logger nmo_logger_null(void) {
-    nmo_logger logger = {
+nmo_logger_t nmo_logger_null(void) {
+    nmo_logger_t logger = {
         .log = null_log,
         .user_data = NULL,
         .level = NMO_LOG_DEBUG
@@ -41,8 +41,8 @@ nmo_logger nmo_logger_null(void) {
     return logger;
 }
 
-nmo_logger nmo_logger_custom(nmo_log_fn log, void* user_data, nmo_log_level level) {
-    nmo_logger logger = {
+nmo_logger_t nmo_logger_custom(nmo_log_fn log, void *user_data, nmo_log_level_t level) {
+    nmo_logger_t logger = {
         .log = log,
         .user_data = user_data,
         .level = level
@@ -50,7 +50,7 @@ nmo_logger nmo_logger_custom(nmo_log_fn log, void* user_data, nmo_log_level leve
     return logger;
 }
 
-void nmo_vlog(nmo_logger* logger, nmo_log_level level, const char* format, va_list args) {
+void nmo_vlog(nmo_logger_t *logger, nmo_log_level_t level, const char *format, va_list args) {
     if (logger == NULL || logger->log == NULL) {
         return;
     }
@@ -68,7 +68,7 @@ void nmo_vlog(nmo_logger* logger, nmo_log_level level, const char* format, va_li
     logger->log(logger->user_data, level, buffer);
 }
 
-void nmo_log(nmo_logger* logger, nmo_log_level level, const char* format, ...) {
+void nmo_log(nmo_logger_t *logger, nmo_log_level_t level, const char *format, ...) {
     va_list args;
     va_start(args, format);
     nmo_vlog(logger, level, format, args);
