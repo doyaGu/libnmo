d677374129b9e4713a2e2fb54d20ab3bacc197f7
doyaGu <doyaGu@users.noreply.github.com>
2025-11-12 22:22:57 -0500

    Enhance schema core

--- src/schema/schema.c
+++ src/schema/schema.c
@@ -5,60 +5,59 @@
 
 #include "schema/nmo_schema.h"
 #include <string.h>
-#include <stddef.h>
 
 /**
  * Get field type name
  */
-const char* nmo_field_type_name(nmo_field_type type) {
+const char *nmo_field_type_name(nmo_field_type_t type) {
     switch (type) {
-        case NMO_FIELD_INT8:       return "int8";
-        case NMO_FIELD_UINT8:      return "uint8";
-        case NMO_FIELD_INT16:      return "int16";
-        case NMO_FIELD_UINT16:     return "uint16";
-        case NMO_FIELD_INT32:      return "int32";
-        case NMO_FIELD_UINT32:     return "uint32";
-        case NMO_FIELD_FLOAT:      return "float";
-        case NMO_FIELD_STRING:     return "string";
-        case NMO_FIELD_GUID:       return "guid";
-        case NMO_FIELD_OBJECT_ID:  return "object_id";
-        case NMO_FIELD_STRUCT:     return "struct";
-        case NMO_FIELD_ARRAY:      return "array";
-        default:                   return "unknown";
+    case NMO_FIELD_INT8: return "int8";
+    case NMO_FIELD_UINT8: return "uint8";
+    case NMO_FIELD_INT16: return "int16";
+    case NMO_FIELD_UINT16: return "uint16";
+    case NMO_FIELD_INT32: return "int32";
+    case NMO_FIELD_UINT32: return "uint32";
+    case NMO_FIELD_FLOAT: return "float";
+    case NMO_FIELD_STRING: return "string";
+    case NMO_FIELD_GUID: return "guid";
+    case NMO_FIELD_OBJECT_ID: return "object_id";
+    case NMO_FIELD_STRUCT: return "struct";
+    case NMO_FIELD_ARRAY: return "array";
+    default: return "unknown";
     }
 }
 
 /**
  * Get field type size
  */
-size_t nmo_field_type_size(nmo_field_type type) {
+size_t nmo_field_type_size(nmo_field_type_t type) {
     switch (type) {
-        case NMO_FIELD_INT8:
-        case NMO_FIELD_UINT8:
-            return 1;
-        case NMO_FIELD_INT16:
-        case NMO_FIELD_UINT16:
-            return 2;
-        case NMO_FIELD_INT32:
-        case NMO_FIELD_UINT32:
-        case NMO_FIELD_FLOAT:
-        case NMO_FIELD_OBJECT_ID:
-            return 4;
-        case NMO_FIELD_GUID:
-            return 8;  // Two 32-bit values
-        case NMO_FIELD_STRING:
-        case NMO_FIELD_STRUCT:
-        case NMO_FIELD_ARRAY:
-            return 0;  // Variable size
-        default:
-            return 0;
+    case NMO_FIELD_INT8:
+    case NMO_FIELD_UINT8:
+        return 1;
+    case NMO_FIELD_INT16:
+    case NMO_FIELD_UINT16:
+        return 2;
+    case NMO_FIELD_INT32:
+    case NMO_FIELD_UINT32:
+    case NMO_FIELD_FLOAT:
+    case NMO_FIELD_OBJECT_ID:
+        return 4;
+    case NMO_FIELD_GUID:
+        return 8; // Two 32-bit values
+    case NMO_FIELD_STRING:
+    case NMO_FIELD_STRUCT:
+    case NMO_FIELD_ARRAY:
+        return 0; // Variable size
+    default:
+        return 0;
     }
 }
 
 /**
  * Validate field descriptor
  */
-int nmo_field_descriptor_validate(const nmo_field_descriptor* field) {
+int nmo_field_descriptor_validate(const nmo_field_descriptor_t *field) {
     if (field == NULL) {
         return NMO_ERR_INVALID_ARGUMENT;
     }
@@ -98,7 +97,7 @@ int nmo_field_descriptor_validate(const nmo_field_descriptor* field) {
 /**
  * Validate schema descriptor
  */
-int nmo_schema_descriptor_validate(const nmo_schema_descriptor* schema) {
+int nmo_schema_descriptor_validate(const nmo_schema_descriptor_t *schema) {
     if (schema == NULL) {
         return NMO_ERR_INVALID_ARGUMENT;
     }
