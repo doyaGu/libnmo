6c210861d6b21be4710ecbe5e2a4a2489c3cfac4
doyaGu <doyaGu@users.noreply.github.com>
2025-11-12 22:17:53 -0500

    Improve logger system

--- include/core/nmo_logger.h
+++ include/core/nmo_logger.h
@@ -1,8 +1,9 @@
 #ifndef NMO_LOGGER_H
 #define NMO_LOGGER_H
 
-#include "nmo_types.h"
 #include <stdarg.h>
+
+#include "nmo_types.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -21,12 +22,12 @@ extern "C" {
 /**
  * @brief Log levels
  */
-typedef enum {
-    NMO_LOG_DEBUG,   /**< Debug messages */
-    NMO_LOG_INFO,    /**< Informational messages */
-    NMO_LOG_WARN,    /**< Warnings */
-    NMO_LOG_ERROR,   /**< Errors */
-} nmo_log_level;
+typedef enum nmo_log_level {
+    NMO_LOG_DEBUG, /**< Debug messages */
+    NMO_LOG_INFO,  /**< Informational messages */
+    NMO_LOG_WARN,  /**< Warnings */
+    NMO_LOG_ERROR, /**< Errors */
+} nmo_log_level_t;
 
 /**
  * @brief Log function callback type
@@ -35,30 +36,30 @@ typedef enum {
  * @param level Log level
  * @param message Log message
  */
-typedef void (*nmo_log_fn)(void* user_data, nmo_log_level level, const char* message);
+typedef void (*nmo_log_fn)(void *user_data, nmo_log_level_t level, const char *message);
 
 /**
  * @brief Logger interface
  */
 typedef struct nmo_logger {
-    nmo_log_fn    log;       /**< Log callback */
-    void*         user_data; /**< User-defined context */
-    nmo_log_level level;     /**< Minimum log level */
-} nmo_logger;
+    nmo_log_fn log;      /**< Log callback */
+    void *user_data;     /**< User-defined context */
+    nmo_log_level_t level; /**< Minimum log level */
+} nmo_logger_t;
 
 /**
  * @brief Create stderr logger
  *
  * @return Logger that outputs to stderr
  */
-NMO_API nmo_logger nmo_logger_stderr(void);
+NMO_API nmo_logger_t nmo_logger_stderr(void);
 
 /**
  * @brief Create null logger (discards all messages)
  *
  * @return Null logger
  */
-NMO_API nmo_logger nmo_logger_null(void);
+NMO_API nmo_logger_t nmo_logger_null(void);
 
 /**
  * @brief Create custom logger
@@ -68,7 +69,7 @@ NMO_API nmo_logger nmo_logger_null(void);
  * @param level Minimum log level
  * @return Custom logger
  */
-NMO_API nmo_logger nmo_logger_custom(nmo_log_fn log, void* user_data, nmo_log_level level);
+NMO_API nmo_logger_t nmo_logger_custom(nmo_log_fn log, void *user_data, nmo_log_level_t level);
 
 /**
  * @brief Log message with printf-style formatting
@@ -78,7 +79,7 @@ NMO_API nmo_logger nmo_logger_custom(nmo_log_fn log, void* user_data, nmo_log_le
  * @param format Printf-style format string
  * @param ... Format arguments
  */
-NMO_API void nmo_log(nmo_logger* logger, nmo_log_level level, const char* format, ...);
+NMO_API void nmo_log(nmo_logger_t *logger, nmo_log_level_t level, const char *format, ...);
 
 /**
  * @brief Log message with va_list
@@ -88,7 +89,7 @@ NMO_API void nmo_log(nmo_logger* logger, nmo_log_level level, const char* format
  * @param format Printf-style format string
  * @param args Format arguments
  */
-NMO_API void nmo_vlog(nmo_logger* logger, nmo_log_level level, const char* format, va_list args);
+NMO_API void nmo_vlog(nmo_logger_t *logger, nmo_log_level_t level, const char *format, va_list args);
 
 // Convenience macros
 #define nmo_log_debug(logger, ...) nmo_log(logger, NMO_LOG_DEBUG, __VA_ARGS__)
