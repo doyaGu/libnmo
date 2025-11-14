fd650799a2664102516fe4754139fed12ab401b6
doyaGu <doyaGu@users.noreply.github.com>
2025-11-14 00:42:26 -0500

    Allow saving objects as reference placeholders

--- tests/unit/test_save_pipeline.c
+++ tests/unit/test_save_pipeline.c
@@ -9,7 +9,11 @@
 #include "app/nmo_session.h"
 #include "session/nmo_object_repository.h"
 #include "core/nmo_arena.h"
+#include "format/nmo_header.h"
+#include "format/nmo_header1.h"
+#include "format/nmo_data.h"
 #include "format/nmo_object.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -392,6 +396,81 @@ TEST(save_pipeline, file_info_propagation) {
     nmo_context_release(ctx);
 }
 
+TEST(save_pipeline, reference_only_save) {
+    nmo_context_desc_t desc = {0};
+    nmo_context_t *ctx = nmo_context_create(&desc);
+    ASSERT_NOT_NULL(ctx);
+
+    nmo_session_t *session = nmo_session_create(ctx);
+    ASSERT_NOT_NULL(session);
+
+    nmo_arena_t *arena = nmo_session_get_arena(session);
+    nmo_object_repository_t *repo = nmo_session_get_repository(session);
+
+    nmo_object_t *obj = (nmo_object_t *) nmo_arena_alloc(arena, sizeof(nmo_object_t), sizeof(void *));
+    ASSERT_NOT_NULL(obj);
+    memset(obj, 0, sizeof(nmo_object_t));
+    obj->class_id = 0x0BADF00D;
+    obj->name = "ReferenceObject";
+    obj->arena = arena;
+
+    ASSERT_EQ(NMO_OK, nmo_object_repository_add(repo, obj));
+
+    nmo_file_info_t file_info = {
+        .file_version = 8,
+        .ck_version = 0x13022002,
+        .file_size = 0,
+        .object_count = 1,
+        .manager_count = 0,
+        .write_mode = 0x01
+    };
+    nmo_session_set_file_info(session, &file_info);
+
+    char filepath[256];
+    build_temp_path(filepath, sizeof(filepath), "test_reference_only.nmo");
+    ASSERT_EQ(NMO_OK, nmo_save_file(session, filepath, NMO_SAVE_AS_OBJECTS));
+
+    FILE *fp = fopen(filepath, "rb");
+    ASSERT_NOT_NULL(fp);
+
+    nmo_file_header_t header;
+    ASSERT_EQ(1u, fread(&header, sizeof(header), 1, fp));
+
+    void *hdr1_buf = malloc(header.hdr1_pack_size);
+    ASSERT_NOT_NULL(hdr1_buf);
+    ASSERT_EQ(1u, fread(hdr1_buf, header.hdr1_pack_size, 1, fp));
+
+    void *data_buf = malloc(header.data_pack_size);
+    ASSERT_NOT_NULL(data_buf);
+    ASSERT_EQ(1u, fread(data_buf, header.data_pack_size, 1, fp));
+    fclose(fp);
+
+    nmo_arena_t *parse_arena = nmo_arena_create(NULL, 4096);
+    ASSERT_NOT_NULL(parse_arena);
+
+    nmo_header1_t hdr1 = {0};
+    hdr1.object_count = header.object_count;
+    nmo_result_t parse_result = nmo_header1_parse(hdr1_buf, header.hdr1_pack_size, &hdr1, parse_arena);
+    ASSERT_EQ(NMO_OK, parse_result.code);
+    ASSERT_TRUE((hdr1.objects[0].flags & NMO_OBJECT_REFERENCE_FLAG) != 0);
+
+    nmo_data_section_t data_section = {0};
+    data_section.manager_count = header.manager_count;
+    data_section.object_count = header.object_count;
+    parse_result = nmo_data_section_parse(data_buf, header.data_pack_size, header.file_version, &data_section, NULL, parse_arena);
+    ASSERT_EQ(NMO_OK, parse_result.code);
+    ASSERT_NULL(data_section.objects[0].chunk);
+    ASSERT_EQ(0u, data_section.objects[0].data_size);
+
+    nmo_arena_destroy(parse_arena);
+    free(hdr1_buf);
+    free(data_buf);
+    remove(filepath);
+
+    nmo_session_destroy(session);
+    nmo_context_release(ctx);
+}
+
 TEST_MAIN_BEGIN()
     REGISTER_TEST(save_pipeline, empty_session_fails);
     REGISTER_TEST(save_pipeline, single_object);
@@ -401,4 +480,5 @@ TEST_MAIN_BEGIN()
     REGISTER_TEST(save_pipeline, null_arguments);
     REGISTER_TEST(save_pipeline, large_count);
     REGISTER_TEST(save_pipeline, file_info_propagation);
+    REGISTER_TEST(save_pipeline, reference_only_save);
 TEST_MAIN_END()
