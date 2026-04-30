5f80dd26d3a1346ee2da7b6a1073b443d4eeb889
doyaGu <doyaGu@users.noreply.github.com>
2026-04-30 17:51:51 -0400

    Add OBJ mesh asset edit tests

--- include/object/nmo_asset_edit.h
+++ include/object/nmo_asset_edit.h
@@ -1,12 +1,24 @@
 #ifndef NMO_ASSET_EDIT_H
 #define NMO_ASSET_EDIT_H
 
+#include "format/nmo_obj_parser.h"
 #include "object/nmo_primitive_mesh.h"
 #include "runtime/nmo_workspace.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct nmo_asset_mesh_material_binding {
+    const char *name;
+    nmo_object_id_t material_id;
+} nmo_asset_mesh_material_binding_t;
+
+typedef struct nmo_asset_mesh_import_options {
+    nmo_object_id_t default_material_id;
+    const nmo_asset_mesh_material_binding_t *materials;
+    size_t material_count;
+} nmo_asset_mesh_import_options_t;
 
 NMO_API nmo_status_t nmo_asset_edit_set_material_color(
     nmo_workspace_edit_t *edit,
@@ -21,6 +33,12 @@ NMO_API nmo_status_t nmo_asset_edit_set_primitive_mesh(
     nmo_object_id_t mesh_id,
     nmo_primitive_mesh_t primitive,
     nmo_object_id_t material_id);
+
+NMO_API nmo_status_t nmo_asset_edit_set_obj_mesh(
+    nmo_workspace_edit_t *edit,
+    nmo_object_id_t mesh_id,
+    const nmo_obj_data_t *obj_data,
+    const nmo_asset_mesh_import_options_t *options);
 
 NMO_API nmo_status_t nmo_asset_edit_bind_entity_mesh(
     nmo_workspace_edit_t *edit,
---
+++ tests/unit/test_asset_edit_obj_mesh.c
@@ -0,0 +1,237 @@
+#include "test_framework.h"
+
+#include "core/nmo_arena.h"
+#include "document/nmo_document.h"
+#include "format/nmo_obj_parser.h"
+#include "format/nmo_object.h"
+#include "object/builtin/nmo_mesh_schemas.h"
+#include "object/nmo_asset_edit.h"
+#include "object/nmo_class_ids.h"
+#include "object/nmo_object_edit.h"
+#include "object/nmo_object_query.h"
+#include "runtime/nmo_context.h"
+#include "runtime/nmo_workspace.h"
+
+#include <string.h>
+
+static void create_workspace(
+    nmo_context_t **out_ctx,
+    nmo_document_t **out_doc,
+    nmo_workspace_t **out_workspace)
+{
+    nmo_context_t *ctx = nmo_context_create(&(nmo_context_desc_t){0});
+    ASSERT_NOT_NULL(ctx);
+    nmo_document_t *doc = nmo_document_create(ctx);
+    ASSERT_NOT_NULL(doc);
+    nmo_workspace_t *workspace = NULL;
+    ASSERT_EQ(NMO_OK, nmo_workspace_create(ctx, doc, &workspace));
+    *out_ctx = ctx;
+    *out_doc = doc;
+    *out_workspace = workspace;
+}
+
+static void destroy_workspace(
+    nmo_context_t *ctx,
+    nmo_document_t *doc,
+    nmo_workspace_t *workspace)
+{
+    nmo_workspace_destroy(workspace);
+    nmo_document_destroy(doc);
+    nmo_context_release(ctx);
+}
+
+static nmo_object_t *find_object(nmo_document_t *doc, nmo_object_id_t id)
+{
+    nmo_object_t *object = NULL;
+    if (nmo_object_query_find_first(
+            doc,
+            &(nmo_object_query_t){.object_id = id},
+            &object,
+            NULL) != NMO_OK) {
+        return NULL;
+    }
+    return object;
+}
+
+static void parse_obj_or_fail(
+    nmo_arena_t *arena,
+    const char *text,
+    nmo_obj_data_t *out_data)
+{
+    ASSERT_EQ(NMO_OK, nmo_obj_parse(arena, text, strlen(text), out_data));
+}
+
+TEST(asset_edit_obj_mesh, imports_triangle_from_parsed_obj)
+{
+    nmo_context_t *ctx = NULL;
+    nmo_document_t *doc = NULL;
+    nmo_workspace_t *workspace = NULL;
+    create_workspace(&ctx, &doc, &workspace);
+
+    nmo_workspace_edit_t *edit = NULL;
+    ASSERT_EQ(NMO_OK, nmo_workspace_edit_begin(workspace, "obj mesh", &edit));
+    nmo_object_id_t mesh_id = 0;
+    ASSERT_EQ(NMO_OK,
+              nmo_object_edit_create(
+                  edit,
+                  &(nmo_object_create_desc_t){
+                      .class_id = NMO_CID_MESH,
+                      .name = "TriangleMesh",
+                  },
+                  &mesh_id));
+
+    nmo_arena_t *parse_arena = nmo_arena_create(NULL, 8192);
+    ASSERT_NOT_NULL(parse_arena);
+    nmo_obj_data_t obj = {0};
+    parse_obj_or_fail(
+        parse_arena,
+        "v 0 0 0 1 0 0\n"
+        "v 1 0 0 0 1 0\n"
+        "v 0 1 0 0 0 1\n"
+        "vt 0 0\n"
+        "vt 1 0\n"
+        "vt 0 1\n"
+        "vn 0 0 1\n"
+        "f 1/1/1 2/2/1 3/3/1\n",
+        &obj);
+
+    ASSERT_EQ(NMO_OK, nmo_asset_edit_set_obj_mesh(edit, mesh_id, &obj, NULL));
+    ASSERT_EQ(NMO_OK, nmo_workspace_edit_commit(edit));
+
+    nmo_object_t *mesh_object = find_object(doc, mesh_id);
+    ASSERT_NOT_NULL(mesh_object);
+    const nmo_mesh_state_t *mesh =
+        (const nmo_mesh_state_t *)nmo_object_get_state(mesh_object);
+    ASSERT_NOT_NULL(mesh);
+    ASSERT_EQ(3u, mesh->vertex_count);
+    ASSERT_EQ(1u, mesh->face_count);
+    ASSERT_EQ(0u, mesh->line_count);
+    ASSERT_EQ(1u, mesh->material_group_count);
+    ASSERT_NOT_NULL(mesh->vertices);
+    ASSERT_NOT_NULL(mesh->face_vertex_indices);
+    ASSERT_NOT_NULL(mesh->vertex_colors);
+    ASSERT_EQ(0xFFFF0000u, mesh->vertex_colors[0]);
+    ASSERT_EQ(0xFF00FF00u, mesh->vertex_colors[1]);
+    ASSERT_EQ(0xFF0000FFu, mesh->vertex_colors[2]);
+    ASSERT_EQ(0u, mesh->face_vertex_indices[0]);
+    ASSERT_EQ(1u, mesh->face_vertex_indices[1]);
+    ASSERT_EQ(2u, mesh->face_vertex_indices[2]);
+    ASSERT_FLOAT_EQ(0.5f, mesh->bary_center.x, 0.0001f);
+    ASSERT_FLOAT_EQ(0.5f, mesh->bary_center.y, 0.0001f);
+    ASSERT_FLOAT_EQ(0.0f, mesh->bary_center.z, 0.0001f);
+
+    nmo_arena_destroy(parse_arena);
+    destroy_workspace(ctx, doc, workspace);
+}
+
+TEST(asset_edit_obj_mesh, binds_named_obj_materials)
+{
+    nmo_context_t *ctx = NULL;
+    nmo_document_t *doc = NULL;
+    nmo_workspace_t *workspace = NULL;
+    create_workspace(&ctx, &doc, &workspace);
+
+    nmo_workspace_edit_t *edit = NULL;
+    ASSERT_EQ(NMO_OK, nmo_workspace_edit_begin(workspace, "obj material", &edit));
+    nmo_object_id_t material_id = 0;
+    nmo_object_id_t mesh_id = 0;
+    ASSERT_EQ(NMO_OK,
+              nmo_object_edit_create(
+                  edit,
+                  &(nmo_object_create_desc_t){
+                      .class_id = NMO_CID_MATERIAL,
+                      .name = "Red",
+                  },
+                  &material_id));
+    ASSERT_EQ(NMO_OK,
+              nmo_object_edit_create(
+                  edit,
+                  &(nmo_object_create_desc_t){
+                      .class_id = NMO_CID_MESH,
+                      .name = "MaterialMesh",
+                  },
+                  &mesh_id));
+
+    nmo_arena_t *parse_arena = nmo_arena_create(NULL, 8192);
+    ASSERT_NOT_NULL(parse_arena);
+    nmo_obj_data_t obj = {0};
+    parse_obj_or_fail(
+        parse_arena,
+        "v 0 0 0\n"
+        "v 1 0 0\n"
+        "v 0 1 0\n"
+        "usemtl Red\n"
+        "f 1 2 3\n",
+        &obj);
+
+    nmo_asset_mesh_material_binding_t bindings[] = {
+        {.name = "Red", .material_id = material_id},
+    };
+    ASSERT_EQ(NMO_OK,
+              nmo_asset_edit_set_obj_mesh(
+                  edit,
+                  mesh_id,
+                  &obj,
+                  &(nmo_asset_mesh_import_options_t){
+                      .materials = bindings,
+                      .material_count = 1u,
+                  }));
+    ASSERT_EQ(NMO_OK, nmo_workspace_edit_commit(edit));
+
+    nmo_object_t *mesh_object = find_object(doc, mesh_id);
+    ASSERT_NOT_NULL(mesh_object);
+    const nmo_mesh_state_t *mesh =
+        (const nmo_mesh_state_t *)nmo_object_get_state(mesh_object);
+    ASSERT_NOT_NULL(mesh);
+    ASSERT_EQ(1u, mesh->material_group_count);
+    ASSERT_NOT_NULL(mesh->material_groups);
+    ASSERT_EQ(material_id, mesh->material_groups[0].material_id);
+
+    nmo_arena_destroy(parse_arena);
+    destroy_workspace(ctx, doc, workspace);
+}
+
+TEST(asset_edit_obj_mesh, rejects_wrong_class_material_binding)
+{
+    nmo_context_t *ctx = NULL;
+    nmo_document_t *doc = NULL;
+    nmo_workspace_t *workspace = NULL;
+    create_workspace(&ctx, &doc, &workspace);
+
+    nmo_workspace_edit_t *edit = NULL;
+    ASSERT_EQ(NMO_OK, nmo_workspace_edit_begin(workspace, "obj bad material", &edit));
+    nmo_object_id_t wrong_id = 0;
+    nmo_object_id_t mesh_id = 0;
+    ASSERT_EQ(NMO_OK,
+              nmo_object_edit_create(
+                  edit,
+                  &(nmo_object_create_desc_t){.class_id = NMO_CID_CAMERA, .name = "Camera"},
+                  &wrong_id));
+    ASSERT_EQ(NMO_OK,
+              nmo_object_edit_create(
+                  edit,
+                  &(nmo_object_create_desc_t){.class_id = NMO_CID_MESH, .name = "Mesh"},
+                  &mesh_id));
+
+    nmo_arena_t *parse_arena = nmo_arena_create(NULL, 8192);
+    ASSERT_NOT_NULL(parse_arena);
+    nmo_obj_data_t obj = {0};
+    parse_obj_or_fail(parse_arena, "v 0 0 0\nv 1 0 0\nv 0 1 0\nf 1 2 3\n", &obj);
+
+    ASSERT_NE(NMO_OK,
+              nmo_asset_edit_set_obj_mesh(
+                  edit,
+                  mesh_id,
+                  &obj,
+                  &(nmo_asset_mesh_import_options_t){.default_material_id = wrong_id}));
+
+    nmo_workspace_edit_rollback(edit);
+    nmo_arena_destroy(parse_arena);
+    destroy_workspace(ctx, doc, workspace);
+}
+
+TEST_MAIN_BEGIN()
+REGISTER_TEST(asset_edit_obj_mesh, imports_triangle_from_parsed_obj);
+REGISTER_TEST(asset_edit_obj_mesh, binds_named_obj_materials);
+REGISTER_TEST(asset_edit_obj_mesh, rejects_wrong_class_material_binding);
+TEST_MAIN_END()
